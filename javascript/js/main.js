67a083578910090b21e65ee8e8f78e5ee44c21fd
Adalbertobrant <adalbertobrant@gmail.com>
2020-05-10 15:33:47 -0300

    arrumado o script

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -4,7 +4,7 @@
 alert("Mensagem de Alerta");
 
 var variavel = 100, outra_variavel = 500, soma = 0;
-soma = varial + outra_variavel;
+soma = variavel + outra_variavel;
 
-alert("variavel = "+variavel+"+ outra_variavel = "+soma);
+alert("Soma = "+variavel+" + "+outra_variavel+" = "+soma);
 
