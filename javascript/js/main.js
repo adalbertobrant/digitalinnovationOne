c84a9450f647b31362fae9263171b2f815cf8bae
Adalbertobrant <adalbertobrant@gmail.com>
2020-05-10 15:28:46 -0300

    arquivo de scripts

---
+++ javascript/js/main.js
@@ -0,0 +1,10 @@
+// arquivo javascript
+// comando alert
+
+alert("Mensagem de Alerta");
+
+var variavel = 100, outra_variavel = 500, soma = 0;
+soma = varial + outra_variavel;
+
+alert("variavel = "+variavel+"+ outra_variavel = "+soma);
+
