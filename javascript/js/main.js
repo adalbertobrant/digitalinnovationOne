22b000d3ce2632438378165554260c15a979b2ea
Adalbertobrant <adalbertobrant@gmail.com>
2020-05-10 15:43:19 -0300

    funcoes em strings

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,10 +1,22 @@
-// arquivo javascript
+/* arquivo javascript
 // comando alert
 
-alert("Mensagem de Alerta");
+//alert("Mensagem de Alerta");
 
 var variavel = 100, outra_variavel = 500, soma = 0;
 soma = variavel + outra_variavel;
 
 alert("Soma = "+variavel+" + "+outra_variavel+" = "+soma);
 
+console.log(soma + variavel + outra_variavel);
+*/
+
+var frase = "O Brasil ganhou a copa do mundo em 1970";
+
+// funçao replace
+
+// mudar a palavra Brasil por Jairzinho
+
+var nova_frase = frase.replace("Brasil","Jairzinho");
+
+alert(nova_frase);
